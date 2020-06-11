0b941d6091694772af4507ad323b31e5cb7b86f0
gem007bd <gem007bd@yahoo.com>
2020-06-11 13:06:42 +0200

    add route files

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/users', require('./route/api/user'));
+app.use('/api/auth', require('./route/api/auth'));
+app.use('/api/post', require('./route/api/post'));
+app.use('/api/profile', require('./route/api/profile'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
