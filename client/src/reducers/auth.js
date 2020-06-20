72aa5ef9b96717a89589117a93b5013c3a5375fd
gem007bd <gem007bd@yahoo.com>
2020-06-20 16:47:11 +0200

    add login part

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,11 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -11,7 +18,16 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        isAuthenticated: true,
+        loding: false,
+        user: payload
+      };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -20,6 +36,8 @@ export default function (state = initialState, action) {
         loding: false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
